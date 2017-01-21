9406d1f3f8d8a2cc1c1602c44a3cd51cfc3afe73
WL0416 <liwsuper7@gmail.com>
2017-01-21 12:38:59 +1100

    project add

--- Chapter_8_Table_Cells/Chapter_8_Table_Cells/ViewController.swift
+++ Chapter_8_Table_Cells/Chapter_8_Table_Cells/ViewController.swift
@@ -10,9 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    let cellTableIdentifier = "CellTableIdentifier"
+    
+    let computers = [
+        ["Name" : "MacBook Air", "Color" : "Silver"],
+        ["Name" : "MacBook Pro", "Color" : "Silver"],
+        ["Name" : "iMac", "Color" : "Silver"],
+        ["Name" : "Mac Mini", "Color" : "Silver"],
+        ["Name" : "Mac Pro", "Color" : "Black"]
+    ]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        tableView.registerClass(NameAndColorCell.self,
+                                forCellReuseIdentifier: cellTableIdentifier)
+        let nib = UINib(nibName: "NameAndColorCell", bundle: nil)
+        tableView.registerNib(nib,
+                              forCellReuseIdentifier: cellTableIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +37,21 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        return computers.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath indexPath: NSIndexPath)
+        -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier(
+                cellTableIdentifier, forIndexPath: indexPath)
+                as! NameAndColorCell
+            let rowData = computers[indexPath.row]
+            cell.name = rowData["Name"]!
+            cell.color = rowData["Color"]!
+            return cell }
 
 }
 

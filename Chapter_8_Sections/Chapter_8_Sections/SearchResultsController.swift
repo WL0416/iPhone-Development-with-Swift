e5a9874e425a9cd14e592e0f7861ac9e515b6fc4
WL0416 <liwsuper7@gmail.com>
2017-01-21 12:37:55 +1100

    project add

--- Chapter_8_Sections/Chapter_8_Sections/SearchResultsController.swift
+++ Chapter_8_Sections/Chapter_8_Sections/SearchResultsController.swift
@@ -8,8 +8,17 @@
 
 import UIKit
 
-class SearchResultsController: UITableViewController {
+class SearchResultsController: UITableViewController, UISearchResultsUpdating {
 
+    let sectionsTableIdentifier = "SectionsTableIdentifier"
+    var names:[String: [String]] = [String: [String]]()
+    var keys: [String] = []
+    var filteredNames: [String] = []
+    
+    private let longNameSize = 6
+    private let shortNamesButtonIndex = 1
+    private let longNamesButtonIndex = 2
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -18,6 +27,9 @@ class SearchResultsController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem()
+        
+        tableView.registerClass(UITableViewCell.self,
+                                forCellReuseIdentifier: sectionsTableIdentifier)
     }
 
     override func didReceiveMemoryWarning() {
@@ -34,18 +46,16 @@ class SearchResultsController: UITableViewController {
 
     override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+         return filteredNames.count
     }
 
-    /*
+    
     override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("reuseIdentifier", forIndexPath: indexPath)
-
-        // Configure the cell...
-
+        let cell = tableView.dequeueReusableCellWithIdentifier(
+            sectionsTableIdentifier)! as UITableViewCell
+        cell.textLabel!.text = filteredNames[indexPath.row]
         return cell
     }
-    */
 
     /*
     // Override to support conditional editing of the table view.
@@ -91,5 +101,34 @@ class SearchResultsController: UITableViewController {
         // Pass the selected object to the new view controller.
     }
     */
-
+    
+    // MARK: UISearchResultsUpdating Conformance
+    func updateSearchResultsForSearchController(searchController: UISearchController) {
+        let searchString = searchController.searchBar.text
+        let buttonIndex = searchController.searchBar.selectedScopeButtonIndex
+        filteredNames.removeAll(keepCapacity: true)
+        if !searchString!.isEmpty {
+            let filter: String -> Bool = { name in
+                // Filter out long or short names depending on which
+                // scope button is selected.
+                let nameLength = name.characters.count
+                if (buttonIndex == self.shortNamesButtonIndex
+                    && nameLength >= self.longNameSize)
+                    || (buttonIndex == self.longNamesButtonIndex
+                        && nameLength < self.longNameSize) {
+                    return false
+                }
+                let range = name.rangeOfString(searchString!,
+                                               options: NSStringCompareOptions.CaseInsensitiveSearch)
+                return range != nil
+            }
+            for key in keys {
+                let namesForKey = names[key]!
+                let matches = namesForKey.filter(filter)
+                filteredNames += matches
+            }
+        }
+        
+        tableView.reloadData()
+    }
 }
--- Chapter_8_Sections/Chapter_8_Sections/ViewController.swift
+++ Chapter_8_Sections/Chapter_8_Sections/ViewController.swift
@@ -8,11 +8,41 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let sectionsTableIdentifier = "SectionsTableIdentifier"
+    @IBOutlet weak var tableView: UITableView!
+    var names: [String: [String]]!
+    var keys: [String]!
+    var searchController: UISearchController!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerClass(UITableViewCell.self,
+                                forCellReuseIdentifier: sectionsTableIdentifier)
+        let path = NSBundle.mainBundle().pathForResource(
+            "sortednames", ofType: "plist")
+        let namesDict = NSDictionary(contentsOfFile: path!)
+        names = namesDict as! [String: [String]]
+        keys = (namesDict!.allKeys as! [String]).sort()
+        
+        let resultsController = SearchResultsController()
+        resultsController.names = names
+        resultsController.keys = keys
+        searchController =
+            UISearchController(searchResultsController: resultsController)
+        let searchBar = searchController.searchBar
+        searchBar.scopeButtonTitles = ["All", "Short", "Long"]
+        searchBar.placeholder = "Enter a search term"
+        searchBar.sizeToFit()
+        tableView.tableHeaderView = searchBar
+        searchController.searchResultsUpdater = resultsController
+        
+        tableView.sectionIndexBackgroundColor = UIColor.blackColor()
+        tableView.sectionIndexTrackingBackgroundColor = UIColor.darkGrayColor()
+        tableView.sectionIndexColor = UIColor.whiteColor()
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +50,37 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // MARK: Table View Data Source Methods
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return keys.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   numberOfRowsInSection section: Int) -> Int {
+        let key = keys[section]
+        let nameSection = names[key]!
+        return nameSection.count
+    }
+    
+    func tableView(tableView: UITableView,
+                   titleForHeaderInSection section: Int) -> String? {
+        return keys[section]
+    }
+    func tableView(tableView: UITableView,
+                   cellForRowAtIndexPath indexPath: NSIndexPath)
+        -> UITableViewCell {
+            let cell = tableView.dequeueReusableCellWithIdentifier(sectionsTableIdentifier, forIndexPath:
+                indexPath)
+                as UITableViewCell
+            let key = keys[indexPath.section]
+            let nameSection = names[key]!
+            cell.textLabel!.text = nameSection[indexPath.row]
+            return cell
+    }
+    
+    // This function shows the titles of each group on the right of screen.
+    func sectionIndexTitlesForTableView(tableView: UITableView) -> [String]? {
+        return keys
+    }
 }
 
